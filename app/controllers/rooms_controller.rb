7cff6feb7f40e28266420fc3a5b6a5a1b42ad2dc
minidebu <debudebo3@gmail.com>
2022-02-10 23:01:42 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,13 +3,18 @@ class RoomsController < ApplicationController
     @room=Room.new
   end
   def create  
-    Room.create(room_params)
+    @room=Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
 
   end
 
   private
   def room_params
-    params.require(:room).permit()
+    params.require(:room).permit(:name,user_ids:[] )
   end
 
 end
